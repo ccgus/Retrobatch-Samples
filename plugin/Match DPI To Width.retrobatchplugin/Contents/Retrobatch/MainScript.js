30937ec76686df9c8204e785a2a0a92979639062
August Mueller <gus@flyingmeat.com>
2019-02-08 16:01:59 -0800

    Sample plugin for setting the DPI of an image.

---
+++ plugin/Match DPI To Width.retrobatchplugin/Contents/Retrobatch/MainScript.js
@@ -0,0 +1,45 @@
+module.exports = {
+    
+    inputKeys: ["inputPrintingWidth", "inputUnitName"],
+    
+    attributes: {
+        "inputPrintingWidth": {
+            displayName: "Printing Width",
+            min: 1.00,
+            sliderMin: 1.00,
+            sliderMax: 100.00,
+            default: 10,
+            type: kCIAttributeTypeScalar,
+        },
+        
+        "inputUnitName": {
+            displayName: "Unit",
+            default: "Inches",
+            type: "OTAttributeTypePopUp",
+            "kOTInputKeyControllerValues": ["Inches", "Centimeters",],
+            "kOTInputKeyControllerClassName": "OTPopUpInputKeyController",
+                                        
+        },
+    },
+    
+    processAsset: function(document, rbnode, asset) {
+        
+        var desiredWidth  = rbnode.nodeValues().inputPrintingWidth;
+        var unit          = rbnode.nodeValues().inputUnitName;
+        var widthInPixels = asset.imageWidth();
+        
+        var newDPI = widthInPixels / desiredWidth;
+        
+        if (unit == "Centimeters") {
+            newDPI = newDPI * 2.54;
+        }
+        
+        asset.setDPI(newDPI);
+        
+        return true;
+    },
+    
+};
+
+
+
