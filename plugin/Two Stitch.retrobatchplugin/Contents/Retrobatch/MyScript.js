d79961e95ba13f6c65f7b069eb659764012f57bc
August Mueller <gus@flyingmeat.com>
2019-05-13 09:57:12 -0700

    Added a vertical option.

--- plugin/Two Stitch.retrobatchplugin/Contents/Retrobatch/MyScript.js
+++ plugin/Two Stitch.retrobatchplugin/Contents/Retrobatch/MyScript.js
@@ -2,6 +2,17 @@ module.exports = {
     
     lastAsset: null,
     
+    // Input Keys must always start with 'input'
+    inputKeys: ["inputStitchVertical"],
+    
+    attributes: {
+        "inputStitchVertical": {
+            displayName: "Stitch Vertical",
+            default: 1,
+            type: RBAttributeTypeBoolean,
+        }
+    },
+    
     processAsset: function(document, rbnode, asset) {
         
         if (!this.lastAsset) {
@@ -10,19 +21,33 @@ module.exports = {
         }
         
         if (this.lastAsset) {
-        
-            // Let's make a new big bitmap that will fit both of our images.            
-            var firstImageWidth = this.lastAsset.imageSize().width;
-            var width = firstImageWidth + asset.imageSize().width;
-            var height = Math.max(this.lastAsset.imageSize().height, asset.imageSize().height);
-            var acc = this.lastAsset.emptyImageAccumulatorOfSize(CGSizeMake(width, height));
+            var inputStitchVertical = rbnode.nodeValues().inputStitchVertical;
+            var lastSize = this.lastAsset.imageSize();
+            var thisSize = asset.imageSize();
+            var acc = null;
+            
+ 
+            var secondImage = asset.CIImage();
+            
+            if (inputStitchVertical) {
+                // Let's make a new big bitmap that will fit both of our images.            
+                var firstImageHeight = lastSize.height;
+                var height           = firstImageHeight + thisSize.height;
+                var width            = Math.max(lastSize.width, thisSize.width);
+                acc                  = this.lastAsset.emptyImageAccumulatorOfSize(CGSizeMake(width, height));
+                secondImage          = secondImage.imageByApplyingTransform(CGAffineTransformMakeTranslation(0, firstImageHeight));
+            }
+            else {
+                // Let's make a new big bitmap that will fit both of our images.            
+                var firstImageWidth = lastSize.width;
+                var width           = firstImageWidth + thisSize.width;
+                var height          = Math.max(lastSize.height, thisSize.height);
+                acc                 = this.lastAsset.emptyImageAccumulatorOfSize(CGSizeMake(width, height));
+                secondImage         = secondImage.imageByApplyingTransform(CGAffineTransformMakeTranslation(firstImageWidth, 0));
+            }
             
             // Let's draw our first image.
             acc.drawCIImage(this.lastAsset.CIImage());
-            
-            // And now we want to offset our second image by the width of the first
-            var secondImage = asset.CIImage();
-            secondImage = secondImage.imageByApplyingTransform(CGAffineTransformMakeTranslation(firstImageWidth, 0));
             
             // And then draw that on our image accumulator as well.
             acc.drawCIImage(secondImage);
